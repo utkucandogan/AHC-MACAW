c77752dc15d1b4e04df48a19345429d23d236a4b
Utkucan Doğan <41344400+utkucandogan@users.noreply.github.com>
2022-06-05 10:28:41 +0300

    New version

--- NODE/nodeUsrp.py
+++ NODE/nodeUsrp.py
@@ -1,6 +1,6 @@
 from .node import Node
 
-from adhoccomputing.Generics import ConnectorTypes
+from adhoccomputing.Generics import *
 from adhoccomputing.Networking.PhysicalLayer.UsrpB210OfdmFlexFramePhy import  UsrpB210OfdmFlexFramePhy
 from adhoccomputing.Networking.MacProtocol.CSMA import MacCsmaPPersistent, MacCsmaPPersistentConfigurationParameters
 
@@ -10,11 +10,12 @@ class NodeUsrp(Node):
         super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
         
         # SUBCOMPONENTS
-        macconfig = MacCsmaPPersistentConfigurationParameters(0.5)
+        macconfig     = MacCsmaPPersistentConfigurationParameters(0.5, -51)
+        sdrconfig     = SDRConfiguration(freq =2484000000.0, bandwidth = 20000000, chan = 0, hw_tx_gain = 76.0, hw_rx_gain = 20.0, sw_tx_gain = -12.0)
+        bladerfconfig = SDRConfiguration(freq =900000000.0,  bandwidth = 250000,   chan = 0, hw_tx_gain = 50.0, hw_rx_gain = 20.0, sw_tx_gain = -12.0)
 
-        self.phy  = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, topology=topology)
-        self.mac  = MacCsmaPPersistent("MacCsmaPPersistent", componentinstancenumber, configurationparameters=macconfig,
-                                       uhd=self.phy.ahcuhd, topology=topology)
+        self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, usrpconfig=sdrconfig, topology=topology)
+        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentinstancenumber, configurationparameters=macconfig, uhd=self.phy.ahcuhd, topology=topology)
         
         self.components.append(self.mac)
         self.components.append(self.phy)
