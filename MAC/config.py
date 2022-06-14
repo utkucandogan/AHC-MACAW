7ab16e17418e84de943a767cfe61c21f6ae44e20
Utkucan Doğan <41344400+utkucandogan@users.noreply.github.com>
2022-06-14 17:05:08 +0300

    Various tests

--- MAC/config.py
+++ MAC/config.py
@@ -1,5 +1,5 @@
 class MACAWConfigurationParameters:
-    def __init__(self, slotTime = 50e-3, backoffMin = 2, backoffMax = 64, verbose = False):
+    def __init__(self, slotTime = 100e-3, backoffMin = 2, backoffMax = 64, verbose = False):
         self.slotTime = slotTime
         self.backoffMin = backoffMin
         self.backoffMax = backoffMax
